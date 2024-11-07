a128162203e67ce3241d5918a10430ad30ecb872
Paulinagarciag <102259331+Paulinagarciag@users.noreply.github.com>
2024-11-06 23:57:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Excluye react-router-dom del empaquetado
-    },
-  },
+  plugins: [react()]
 })
